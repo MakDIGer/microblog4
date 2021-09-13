2279362c970b75e78f1adce59d8090e0ecb92756
Makarin Nikolay <makarin@kgdnet.ru>
2021-09-13 23:48:31 +0200

    MainController added in index-router.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const MainController = require('./../app/controllers/MainController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', MainController);
 
 module.exports = router;
