35873faafce18c576bdb91dd75399181099dfd6b
Makarin Nikolay <makarin@kgdnet.ru>
2021-09-18 23:20:51 +0200

    The feedback route added in main router.

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const HomeController = require('./../app/controllers/HomeController'),
-      AboutController = require('./../app/controllers/AboutController');
+      AboutController = require('./../app/controllers/AboutController'),
+      FeedbackController = require('./../app/controllers/FeedbackController');
 
 /* GET home page. */
 router.get('/', HomeController);
 router.get('/about', AboutController);
+router.get('/feedback', FeedbackController);
 
 module.exports = router;
