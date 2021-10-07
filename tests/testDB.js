abba883382771c88fc1d7936660d4c2a9911c95c
Makarin Nikolay <makarin@kgdnet.ru>
2021-10-07 22:27:42 +0200

    Added test for post-table of a database.

--- tests/testDB.js
+++ tests/testDB.js
@@ -28,6 +28,63 @@ describe('DB', () => {
                 const numCategories = categories.length;
 
                 should.not.equal(numCategories, 0);
+
+                conn.disconnect;
+            }
+        });
+        it('posts', () => {
+            main().catch(err => console.log(err));
+
+            async function main() {
+                try {
+                    await mongoose.connect(keys.mongoURI)
+                } catch (e) {
+                    console.log(e);
+                }
+
+                const conn = mongoose.connection;
+
+                const postSchema = new mongoose.Schema({
+                    slug: {
+                        type: String,
+                        required: true,
+                        min: 4
+                    },
+                    title: {
+                        type: String,
+                        required: true,
+                        min: 4,
+                        unique: true
+                    },
+                    category: {
+                        ref: 'categories',
+                        type: mongoose.Schema.Types.ObjectId,
+                        required: true
+                    },
+                    date: {
+                        type: Date,
+                        default: Date.now
+                    },
+                    prevText: {
+                        type: String,
+                        min: 4
+                    },
+                    text: {
+                        type: String,
+                        required: true
+                    },
+                    tags: [{
+                        type: String
+                    }]
+                });
+
+                const Post = conn.model('Post', postSchema);
+                const posts = await Post.find();
+                const numPosts = posts.length;
+
+                should.not.equal(numPosts, 0);
+
+                conn.disconnect;
             }
         })
     })
